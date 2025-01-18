38be93b7b57e3648443961a02882ebdaffdc2e50
Daniel Gilbert <ich@danielgilbert.de>
2025-01-18 09:40:55 +0100

    Implements SilkNetDriver skeleton

---
+++ Terminal.Gui/ConsoleDrivers/SilkNetDriver/SilkNetDriver.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Terminal.Gui.ConsoleDrivers.SilkNetDriver;
+public class SilkNetDriver : ConsoleDriver
+{
+    public SilkNetDriver ()
+    {
+        ClearContents ();
+    }
+
+    public override void End ()
+    {
+        // throw new NotImplementedException ();
+        Console.WriteLine ("End");
+       this.End ();
+    }
+
+    public override bool GetCursorVisibility (out CursorVisibility visibility)
+    {
+        visibility = CursorVisibility.Default;
+        Console.WriteLine ("GetCursorVisibility");
+        return true;
+    }
+
+    public override MainLoop Init ()
+    {
+        Console.WriteLine ("Init");
+        return new MainLoop(new SilkNetMainLoop ());
+    }
+
+    public override void SendKeys (char keyChar, ConsoleKey key, bool shift, bool alt, bool ctrl)
+    {
+        Console.WriteLine ("SendKeys");
+        this.SendKeys (keyChar, key, shift, alt, ctrl);
+    }
+
+    public override bool SetCursorVisibility (CursorVisibility visibility)
+    {
+        Console.WriteLine ("SetCursorVisibility");
+        return this.SetCursorVisibility (visibility);
+    }
+
+    public override void Suspend ()
+    {
+        Console.WriteLine ("Suspend");
+        //throw new NotImplementedException ();
+    }
+
+    public override void UpdateCursor ()
+    {
+        Console.WriteLine ("UpdateCursor");
+        //throw new NotImplementedException ();
+    }
+
+    public override bool UpdateScreen ()
+    {
+        Console.WriteLine ("UpdateScreen");
+        //throw new NotImplementedException ();
+        return true;
+    }
+
+    public override void WriteRaw (string ansi)
+    {
+        Console.WriteLine ("WriteRaw");
+        //throw new NotImplementedException ();
+    }
+
+    private AnsiResponseParser _parser = new ();
+
+    /// <inheritdoc />
+    internal override IAnsiResponseParser GetParser () => _parser;
+}
---
+++ Terminal.Gui/ConsoleDrivers/SilkNetDriver/SilkNetMainLoop.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Terminal.Gui.ConsoleDrivers.SilkNetDriver;
+internal class SilkNetMainLoop : IMainLoopDriver
+{
+    public bool EventsPending ()
+    {
+        // throw new NotImplementedException ();
+
+        return false;
+    }
+
+    public void Iteration ()
+    {
+       // throw new NotImplementedException ();
+    }
+
+    public void Setup (MainLoop mainLoop)
+    {
+       // throw new NotImplementedException ();
+    }
+
+    public void TearDown ()
+    {
+       // throw new NotImplementedException ();
+    }
+
+    public void Wakeup ()
+    {
+       // throw new NotImplementedException ();
+    }
+}
